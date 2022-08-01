5dd7b4f89a6706167b8723535a9360f42978685c
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-01 20:44:03 +0200

    feat: added retrofit interface to fetch quotes by anime, character or random.

---
+++ app/src/main/java/com/myasser/testapi/retrofit/AnimeQuoteRetrofit.kt
@@ -0,0 +1,16 @@
+package com.myasser.testapi.retrofit
+
+import com.myasser.testapi.models.AnimeQuote
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AnimeQuoteRetrofit {
+    @GET("quotes/{character?name}")
+    fun getQuoteByCharacter(@Path("character?name") characterName:String): Call<ArrayList<AnimeQuote>>
+    @GET("quotes/{anime?title}")
+    fun getQuoteByAnimeTitle(@Path("anime?title") animeTitle:String): Call<ArrayList<AnimeQuote>>
+    @GET("/random")
+    fun getRandomQuote():Call<AnimeQuote>
+}
+//todo: try to fetch quote by anime title and character name
