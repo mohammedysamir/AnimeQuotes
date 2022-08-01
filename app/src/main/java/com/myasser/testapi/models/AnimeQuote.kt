773717add5d23b949e886f73db5699e4fb153c46
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-02 00:00:15 +0200

    refactor: change parameters name to match json

--- app/src/main/java/com/myasser/testapi/models/AnimeQuote.kt
+++ app/src/main/java/com/myasser/testapi/models/AnimeQuote.kt
@@ -1,3 +1,3 @@
 package com.myasser.testapi.models
 
-data class AnimeQuote(var animeName:String,var animeCharacter:String,var quote: String)
+data class AnimeQuote(var anime:String,var character:String,var quote: String)
