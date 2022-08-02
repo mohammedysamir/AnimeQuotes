f9a7812066c9df75455aa767bd9b13b5b93345ac
mohammed yasser <47453799+mohammedysamir@users.noreply.github.com>
2022-08-03 00:44:27 +0200

    Merge pull request #1
    * feat: created initial quote holder card
    
    * feat: added recycler view adapter with listener.
    
    * feat: added activity to show list of quotes fetched.
    
    * refactor: added margin for quote holder card for better view.
    
    * feat: added initial UI and logic to display anime quote
    
    * feat: added todos and onClick template function.
    
    * feat: added retrofit interface to fetch quotes by anime, character or…
    
    * feat: added initial logo for the app.
    
    * delete: delete old logo resources .webp
    
    * fix: fix issue that base url wasn't end with /
    
    * refactor: change backgrounds to white to support only light mode in c…
    
    * refactor: change styles to fit dark theme that is not supported :"D
    
    * refactor: change parameters name to match json
    
    * refactor: replace old params names for AnimeQuote to new ones
    
    * refactor: change theme app bar color
    
    * feat: added card view for quote and add quote braces.
    
    * refactor: added the character name to the card and format it for bett…
    
    * remove completed todos.
    
    * fix: fixed the issue of fetching quotes by anime or character by appl…
    
    * fix: fixed the issue of fetching quotes, app was navigating first the…
    
    * can't fetch both by name and title.

--- app/src/main/java/com/myasser/testapi/MainActivity.kt
+++ app/src/main/java/com/myasser/testapi/MainActivity.kt
@@ -1,12 +1,140 @@
 package com.myasser.testapi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.myasser.testapi.models.AnimeQuote
+import com.myasser.testapi.retrofit.AnimeQuoteRetrofit
+import com.myasser.testapi.screens.QuoteActivity
+import com.myasser.testapi.screens.ViewQuotesActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var retrofitBuilder: Retrofit
+    private lateinit var animeInterface: AnimeQuoteRetrofit
+
+    companion object {
+        lateinit var incomingResponse: ArrayList<AnimeQuote>
+    }
+
+    init {
+        incomingResponse = ArrayList()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //todo: define retrofit builder, fetch quote and send them to other activities
+        retrofitBuilder = Retrofit.Builder().baseUrl("https://animechan.vercel.app/api/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        animeInterface = retrofitBuilder.create(AnimeQuoteRetrofit::class.java)
+
+        //set button listeners
+        findViewById<AppCompatButton>(R.id.searchButton).setOnClickListener(this)
+        findViewById<AppCompatButton>(R.id.searchRandomButton).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.searchButton -> {
+                val animeTextView = findViewById<TextView>(R.id.byAnimeNameTextField)
+                val characterTextView = findViewById<TextView>(R.id.byCharacterNameTextField)
+                if (animeTextView.text.isEmpty()) {
+                    val characterName = characterTextView.text.toString().trim()
+                    fetchQuoteByName(characterName, false)
+                } else if (animeTextView.text.isNotEmpty()) {
+                    //search by anime name
+                    val animeTitle = animeTextView.text.toString().trim()
+                    fetchQuoteByName(animeTitle, true)
+                }
+            }
+            R.id.searchRandomButton -> {
+                val call: Call<AnimeQuote> = animeInterface.getRandomQuote()
+                call.enqueue(object : Callback<AnimeQuote> {
+                    override fun onResponse(
+                        call: Call<AnimeQuote>?,
+                        response: Response<AnimeQuote>?
+                    ) {
+                        val intent = Intent(v.context, QuoteActivity::class.java).apply {
+                            putExtra("Anime Name", response?.body()?.anime)
+                            putExtra("Character Name", response?.body()?.character)
+                            putExtra("Quote", response?.body()?.quote)
+                        }
+                        startActivity(intent)
+                    }
+
+                    override fun onFailure(call: Call<AnimeQuote>?, t: Throwable?) {
+                        Toast.makeText(v.context, "Sorry, something went wrong!", Toast.LENGTH_LONG)
+                            .show()
+                    }
+
+                })
+            }
+        }
+    }
+
+    private fun fetchQuoteByName(name: String, isAnimeTitle: Boolean) {
+        val viewQuoteIntent = Intent(this@MainActivity, ViewQuotesActivity::class.java).apply {
+            putExtra("Anime Name", name)
+        }
+        if (isAnimeTitle) {
+            val call: Call<ArrayList<AnimeQuote>> =
+                animeInterface.getQuoteByAnimeTitle(animeTitle = name)
+            call.enqueue(object : Callback<ArrayList<AnimeQuote>> {
+                override fun onResponse(
+                    call: Call<ArrayList<AnimeQuote>>?,
+                    response: Response<ArrayList<AnimeQuote>>?
+                ) {
+                    if (response?.isSuccessful!!) {
+                        incomingResponse = response.body()!!
+                        startActivity(viewQuoteIntent)
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<AnimeQuote>>?, t: Throwable?) {
+                    Log.i("Error", t.toString())
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Couldn't find quotes for selected anime title",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            })
+        } else {
+            val call: Call<ArrayList<AnimeQuote>> =
+                animeInterface.getQuoteByCharacter(characterName = name)
+            call.enqueue(object : Callback<ArrayList<AnimeQuote>> {
+                override fun onResponse(
+                    call: Call<ArrayList<AnimeQuote>>?,
+                    response: Response<ArrayList<AnimeQuote>>?
+                ) {
+                    if (response?.isSuccessful!!) {
+                        incomingResponse = response.body()!!
+                        startActivity(viewQuoteIntent)
+
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<AnimeQuote>>?, t: Throwable?) {
+                    Log.i("Error", t.toString())
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Couldn't find quotes for selected character",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            })
+        }
+
     }
 }
 /*
---
+++ app/src/main/java/com/myasser/testapi/screens/QuoteActivity.kt
@@ -0,0 +1,19 @@
+package com.myasser.testapi.screens
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.myasser.testapi.R
+import com.myasser.testapi.models.AnimeQuote
+
+class QuoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_quote)
+        findViewById<TextView>(R.id.animeName).text = intent.getStringExtra("Anime Name")
+        findViewById<TextView>(R.id.characterName).text = "— ${intent.getStringExtra("Character Name")}"
+        findViewById<TextView>(R.id.animeQuote).text = "❝ ${intent.getStringExtra("Quote")} ❞"
+    }
+}
