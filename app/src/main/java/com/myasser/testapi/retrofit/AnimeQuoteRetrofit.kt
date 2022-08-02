6dd7f224a266c74b6b98343ab98b8597b8544d8e
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-02 21:12:07 +0200

    fix: fixed the issue of fetching quotes by anime or character by applying @Query

--- app/src/main/java/com/myasser/testapi/retrofit/AnimeQuoteRetrofit.kt
+++ app/src/main/java/com/myasser/testapi/retrofit/AnimeQuoteRetrofit.kt
@@ -3,14 +3,16 @@ package com.myasser.testapi.retrofit
 import com.myasser.testapi.models.AnimeQuote
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface AnimeQuoteRetrofit {
-    @GET("quotes/{character}")
-    fun getQuoteByCharacter(@Path("character") characterName:String): Call<ArrayList<AnimeQuote>>
-    @GET("quotes/{anime}")
-    fun getQuoteByAnimeTitle(@Path("anime") animeTitle:String): Call<ArrayList<AnimeQuote>>
+    @GET("quotes/character")
+    fun getQuoteByCharacter(@Query("name") characterName: String): Call<ArrayList<AnimeQuote>>
+
+    @GET("quotes/anime")
+    fun getQuoteByAnimeTitle(@Query("title") animeTitle: String): Call<ArrayList<AnimeQuote>>
+
     @GET("random")
-    fun getRandomQuote():Call<AnimeQuote>
+    fun getRandomQuote(): Call<AnimeQuote>
 }
 //todo: try to fetch quote by anime title and character name
